1122942958ab189ad0c57dfa60d5ed2c5df0b1d2
matheusjuniorfree <103582519+matheusjuniorfree@users.noreply.github.com>
2024-07-31 07:40:56 -0300

    ...

--- script.js
+++ script.js
@@ -8,4 +8,24 @@ document.addEventlistener('DOMContentLoaded',()=>{
 
 //Contador de dias para o inicio das aulas
 cont startDate= new Date('2024-08-01');//defina a data de inicio das aulas
-const daysleft=Math.ceil(timeDiff/(1000*60*60*24))
+const daysleft=Math.ceil(timeDiff/(1000*60*60*24));
+
+const countdownElement=document.createElement('p');
+countdownElement.textcontent='faltam ${daysLeft}dias para o inicio das aulas!';
+document.querySelector('.card').appendChild(countdownElement);
+
+//Alterar a imagem ao clicar
+const image=document.querySelector('img');
+image.addEventlistener('click',()=>{
+    image.src=voltaaaulas.jpg;
+});
+
+//Mostrar uma mensagem de boas-vindas com animação
+const card=document.querySelector('.card');
+card.style.opacity=D;
+card.style.transition='opacity 2s';
+
+setTimeout(()=>{
+    card.style.opacity=1;
+},100);
+});
